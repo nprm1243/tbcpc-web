0fbac0429ffdba8b995678f18d80e5bbb5468b56
Nghi Gia <74197138+nprm1243@users.noreply.github.com>
2023-10-29 09:30:04 +0700

    Update trongso.cpp

--- edu2023-2024/number-theory-prime/trongso.cpp
+++ edu2023-2024/number-theory-prime/trongso.cpp
@@ -5,25 +5,25 @@ const int MX = 1e5 + 5;
 bool flag[MX];
 
 int main() {
-  ios::sync_with_stdio(false);
-  cin.tie(0);
+    ios::sync_with_stdio(false);
+    cin.tie(0);
     freopen("trongso.inp", "r", stdin);
     freopen("trongso.out", "w", stdout);
-  int n;
-  cin >> n;
-  for (int a; n--;) {
-    cin >> a;
-    flag[a] = true;
-  }
-  int ans = 0;
-  for (int i = 1; i < MX; ++i) {
-    int d = 0;
-    for (int j = i; j < MX; j += i) {
-      if (flag[j]) d = __gcd(d, j);
+    int n;
+    cin >> n;
+    for (int a; n--;) {
+        cin >> a;
+        flag[a] = true;
     }
-    if (d == i) ++ans;
-  }
-  cout << ans << '\n';
+    int ans = 0;
+    for (int i = 1; i < MX; ++i) {
+        int d = 0;
+        for (int j = i; j < MX; j += i) {
+            if (flag[j]) d = __gcd(d, j);
+        }
+        if (d == i) ++ans;
+    }
+    cout << ans << '\n';
 
-  return 0;
+    return 0;
 }
